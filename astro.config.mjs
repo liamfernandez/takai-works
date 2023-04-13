7f33e9ebe15deea21832724096e2f0e4bd795bf2
Liam Fernandez <liamdfernandez@gmail.com>
2023-04-13 02:22:53 -0700

    Add vercel adapter in order to enable analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import preact from "@astrojs/preact";
+
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()]
+  integrations: [tailwind(), preact()],
+  output: "static",
+  adapter: vercel({
+    analytics: true
+  })
 });
