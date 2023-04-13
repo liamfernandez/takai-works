8c1012aee2414a2c268173f7fb8c623d2895536a
Liam Fernandez <liamdfernandez@gmail.com>
2023-04-13 01:23:10 -0700

    Fix center of arrows. Start optimizing responsive design for mobile

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -6,9 +6,11 @@ export default function Gallery(): JSX.Element {
 
     const orderOfPics: number[] = [1,2,3,4,5,6,7,8,9,10,11];
     const [focusedPic, setFocusedPic] = useState<number>(3); //default center pic is 3
+    const [showLeft, setShowLeft] = useState<boolean>(false);
+    const [showRight, setShowRight] = useState<boolean>(true);
 
-    const leftArrowClass:string = (focusedPic === 3) ? 'opacity-0 pointer-events-none' : '';
-    const rightArrowclass:string = (focusedPic === 9) ? 'opacity-0 pointer-events-none' : '';
+    const leftArrowClass:string = (!showLeft) ? 'opacity-0 pointer-events-none' : '';
+    const rightArrowclass:string = (!showRight) ? 'opacity-0 pointer-events-none' : '';
     
 
     useEffect(() => {
@@ -22,31 +24,44 @@ export default function Gallery(): JSX.Element {
 
 
     return (
-            <ul id={`gallery`} class="carousel carousel-center overflow-x-hidden" onScroll={() => {
+            <ul id={`gallery`} class="carousel carousel-center md:overflow-x-hidden" onScroll={() => {
+                let gallery = document.getElementById(`gallery`)
+                if (gallery?.scrollLeft < 300) {
+                    setShowLeft(false);
+                }
+                else if (!showLeft) {
+                    setShowLeft(true);
+                }
+
+                if (gallery?.scrollWidth - (gallery?.scrollLeft + gallery?.clientWidth) < 300) {
+                    setShowRight(false);
+                } else if (!showRight) {
+                    setShowRight(true);
+                }
             }}>
-                <div id={`left and right buttons`} class="absolute flex justify-between transform -translate-y-1/2 left-6 right-6 2xl:top-[36%] top-[37%]">
+                <div id={`left and right buttons`} class="hidden absolute md:flex justify-between left-6 right-6 self-center">
                     <button id={`left-arrow`} class={leftArrowClass} onClick={() => {
                         let gallery = document.getElementById(`gallery`)
                         if (gallery?.scrollLeft > 300) {
                             gallery.scrollLeft -= 400
-                            setFocusedPic(focusedPic - 1)
+                            // setFocusedPic(focusedPic - 1)
                         }
-                        console.log(focusedPic)
                     }}>
                         <img src={`/left-arrow.svg`} className={`w-[20px]`}/>
                     </button>
                     <button id={`right-arrow`} className={rightArrowclass} onClick={() => {
                         let gallery = document.getElementById(`gallery`)
                         gallery.scrollLeft += 400
-                        setFocusedPic(focusedPic + 1);
+                        // console.log(gallery?.scrollLeft)
+                        // setFocusedPic(focusedPic + 1);
                     }}>
                         <img src={`/right-arrow.svg`} className={`w-[20px]`}/>
                     </button>
                 </div>
                 {orderOfPics.map((num:number) => {
                     return (
-                        <li id={`${prefixPath}${num}`} className={`mx-[1px] carousel-item`}>
-                            <img src={`./assets/${prefixPath}${num}.jpg`} className={`2xl:h-[22.5em] h-[302px]`}/>
+                        <li id={`${prefixPath}${num}`} className={`mx-[1px] carousel-item w-full md:w-fit`}>
+                            <img src={`./assets/${prefixPath}${num}.jpg`} className={`2xl:h-[22.5em] md:h-[302px] h-fit`}/>
                         </li>
                     );
                 })}
